dd35f15547c26475c98599b03759d3a7f4b62b16
sbuisson <sbuisson@xebia.fr>
2017-07-11 00:33:25 +0200

    step 3
    step 4
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    main
    
    Update Jenkinsfile
    
    ded
    Update Jenkinsfile
    
    dzd
    Update Jenkinsfile
    
    rg'rg'
    Update Jenkinsfile
    
    sdd
    Update Jenkinsfile
    
    fefefe
    Update Jenkinsfile
    
    dsdsd
    Update Jenkinsfile
    
    fefe
    dsq
    
    dsq
    
    dsq
    
    Update pom.xml
    
    ddzdz
    dsd
    
    dsdsds
    sds
    
    fds
    sss
    
    dfsfs
    Update Jenkinsfile
    
    fefzfez
    Update Jenkinsfile
    
    ;m;
    Update Jenkinsfile
    
    dzda
    Update Jenkinsfile
    
    dsq
    Update pom.xml
    
    vdvd
    Update pom.xml
    
    vdv
    Create MainKotlin.kt
    
    cedc
    all
    
    all
    
    all
    
    all
    
    all
    
    all
    
    jojpo
    
    jojpo
    
    jojpo
    
    jojpovdsvsdv
    
    jojpovdsvsdv
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvcz
    
    jojpovdsvsdvczcezcz
    
    jojpovdsvsdvczcezczcs
    
    dzdazd
    
    dzdazd
    
    dzdazd
    
    dzdazd
    
    dzdazd
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzef
    
    dzdazdfzefsq
    
    dzdazdfzefsq
    
    dzdazdfzefsq
    
    dzdazdfzefsq
    
    dzdazdfzefsq
    
    dzdazdfzefsq
    
    dzdazdfzefsq
    
    dzdazdfzefsq
    
    dzdazdfzefsqecece
    
    dzdazdfzefsqecece
    
    dzdazdfzefsqecece
    
    dzdazdfzefsqecece
    
    dzdazdfzefsqececesz
    
    dzdazdfzefsqececeszfzf
    
    dzdazdfzefsqececeszfzf
    
    dzdazdfzefsqececeszfzf
    
    dzdazdfzefsqececeszfzf
    
    dzdazdfzefsqececeszfzf
    
    dzdazdfzefsqececeszfzf
    
    dzdazdfzecs
    
    dzdazdfzecs
    
    dzdazdfzecs
    
    dzdazdfzecs
    
    dzdazdfzecs
    
    dzdazdfzecs
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    dzdazdfzecss
    
    ddz
    
    ddz
    
    ddzgrge
    
    ddzgrge
    
    ddzgrge
    
    cz
    
    czsq
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    csvd
    
    sqs
    
    sqsq
    
    sqsq
    
    dz
    
    dz
    
    dz
    
    xa
    
    xa
    
    xa
    
    csdcs
    
    xs
    
    fds
    
    xcx
    
    xsxs
    
    cxcw
    
    cxcw
    
    cxcwko
    
    cxcwkocxw
    
    beau jenkinsFile
    
    beau jenkinsFile
    
    beau jenkinsFile
    
    beau jenkinsFile
    
    beau jenkinsFile
    
    beau jenkinsFile
    
    beau jenkinsFile
    
    beau jenkinsFile
    
    mutations
    
    mutations
    
    report
    
    report
    
    report
    
    report
    
    report
    
    report
    
    other sonar try

---
+++ src/test/java/some/HelloKotlin.kt
@@ -0,0 +1,22 @@
+package some
+
+import org.junit.Test
+import some.Hello
+
+/**
+ * Created by sbuisson on 21/09/2017.
+ */
+
+
+/**
+ * Created by sbuisson on 21/09/2017.
+ */
+
+class HelloKotlin{
+    @Test
+     fun hello(){
+
+         Hello().hello();
+         World().world();
+    }
+}
